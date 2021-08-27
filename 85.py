175ab726dd29191dc69fa12846925f5900976161
Justin Chambers <justmcham@gmail.com>
2021-08-27 12:50:26 -0400

    Added guardian

--- 85.py
+++ 85.py
@@ -8,7 +8,10 @@ cnt = 0
 #Search for email addresses
 for line in fh:
     if line.startswith('From '):
-        line = line.rstrip().split()
-        print(line[1])
+        line = line.rstrip()
+        if len(line) < 1 :
+            continue
+        words = line.split()
+        print(words[1])
         cnt = cnt + 1
 print('There were', cnt, 'lines in the file with From as the first word')
