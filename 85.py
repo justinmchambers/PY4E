956fcdf264d2d9f12c32b3b7fe39f1b9652dccaa
Justin Chambers <justmcham@gmail.com>
2021-08-27 12:30:05 -0400

    Ex 8.5

---
+++ 85.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+cnt = 0
+
+#Search for email addresses
+for line in fh:
+    if line.startswith('From:'):
+        continue
+    if line.startswith('From'):
+        line = line.rstrip().split()
+        print(line[1])
+        cnt = cnt + 1
+print('There were', cnt, 'lines in the file with From as the first word')
