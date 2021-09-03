ed9978fd95acd82a20841b731d01e5c105142174
Justin Chambers <justmcham@gmail.com>
2021-09-03 13:52:29 -0400

    Counting emails with SQLite

--- SQL/email.py
+++ SQL/email.py
@@ -1,11 +1,12 @@
 import sqlite3
 import re
 
+#Establish connection + cursor sqlite
 conn = sqlite3.connect('email.sqlite')
 cur = conn.cursor()
 
+#Drop + create table
 cur.execute('DROP TABLE IF EXISTS Counts')
-
 cur.execute('''
 CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
@@ -14,12 +15,15 @@ if len(f) < 1 :
     f = 'mbox-short.txt'
 fh = open(f)
 
+#Read file, find emails
 for line in fh:
     if not line.startswith('From: '):
         continue
     words = line.split()
     email = words[1]
     orgl = re.findall('.*@(.+)', email)
+#REGEX creates a list - iterate through for string values inside
+#Check if domain already in table, add it or update the value by 1
     for org in orgl:
         cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
         row = cur.fetchone()
@@ -35,5 +39,5 @@ sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
-    
+
 cur.close()
